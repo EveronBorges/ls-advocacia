876b265419c7ddc79b8afa1df877da59bf1f99c9
Éveron Borges <everon.borges@gmail.com>
2021-10-27 20:42:30 -0300

    Primeiro Envio

---
+++ src/Inicio.jsx
@@ -0,0 +1,20 @@
+import bg from "./images/top-ls-advocacia.jpg";
+import logo from "./images/logo-ls-advocacia.png";
+
+const Inicio = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${bg})`,
+        minHeight: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        boxShadow: `inset 0 0 0 2000px rgba( 220, 20, 60, 0.5)`,
+      }}
+    >
+      <img src={logo} />
+    </div>
+  );
+};
+
+export default Inicio;
